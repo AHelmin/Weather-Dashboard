8b62e01dfb3237def20404bffeec07d13140476b
helm0127 <helm0127@morris.umn.edu>
2024-01-11 00:31:12 -0600

    renamed images, added css, added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
 //OFFIC HOUR QUESTIONS
-//how to 
+//ask about calling functions in the middle of another function
 
 
 //TODO
-//search bar make event listener capture .val()--DONE
-//search button add event listener--DONE BUT STILL NEED TO FINSIH ONCE I CAN ACCESS WEATHER DATA
-//fetch api from open weather or whatever for search query--LOCATION DATA FETCHED, UNABLE TO FETCH WEATHER DATA
-//save cities searched for in buttons below search
-//openweathermap.org---need to create an account for api key--DONE
-//first need lat and longitude of city--use geocoding api--DONE
-//second can use the lat/long to call 5 day/3 hour forecast has 5 days worth of a forecast each 3 hours
-//we get one of the 3 hour forecasts and call it a day .split().pop()?
 //https://api.openweathermap.org/geocoding?appid=859ytt99234r(api-key)
-//grab long latitude from first api and give them var names--DONE
-//use the weather data to gen the weather 
-//determine how to grab data and write cards
 //finish css to style page
 //my api key === 6c31869795f0dd512d210bae2ff7b924
 
@@ -28,17 +17,6 @@ $(document).ready(function () {
     const cardContainer = $('#five-day');
     const cardBody = $('#card-body');
     let savedSearches = ['', '', '', '', '', '', '', ''];
-
-    // function updateSavedSearches(newSearch) {                 
-    //     savedSearches = savedSearches.map( function(hourBlock) {
-    //         if (hourBlock.hour !==newHourBlock.hour) return hourBlock
-    //         hourBlock.message = newHourBlock.message
-    //         return hourBlock
-    //     })
-    //     updateStorage();
-    //     displayCalendar();
-    //     }
-
 
     function checkForSavedData() {
         const savedData = localStorage.getItem('savedSearches');
@@ -90,29 +68,41 @@ $(document).ready(function () {
         var humidity = data.list[2].main.humidity;
         var windSpeed = data.list[2].wind.speed;
         console.log(cloudStatus);
+       getIcon(cloudStatus);
         var inputVal = cityInput.val();
-        currentDay.children().eq(0).text(`${inputVal} (${adjustedDate})`);
-        currentDay.children().eq(1).text(`Temp: ${tempF.toFixed(2)}°F`);
-        currentDay.children().eq(2).text(`Wind: ${windSpeed} MPH`);
-        currentDay.children().eq(3).text(`Humidity: ${humidity}%`);
+        currentDay.append(`<h2>${inputVal} (${adjustedDate}) ${iconCode}</h2><p>Temp: ${tempF.toFixed(2)}°F</p><p>Wind: ${windSpeed} MPH</p><p>Humidity: ${humidity}%</p>`)
         var j = 2;
         for (var i = 0; i < 5; i++) {
             var dateTime = data.list[j].dt_txt;
             var adjustedDate = dayjs(dateTime).format('M/D/YYYY')
             var cloudStatus = data.list[j].weather[0].main;
+            getIcon(cloudStatus);
             var temp = data.list[j].main.temp;
             var tempF = (temp - 273.15) * 9 / 5 + 32;
             var humidity = data.list[j].main.humidity;
             var windSpeed = data.list[j].wind.speed;
-            cardContainer.children().eq(i).children().eq(0).append(`<h5>${adjustedDate}</h5><p>Temp: ${tempF.toFixed(2)}°F</p><p>Wind: ${windSpeed} MPH</p><p>Humidity: ${humidity}%</p>`).addClass('card-bg-blue');
+            cardContainer.children().eq(i).children().eq(0).append(`<h5>${adjustedDate}</h5>${iconCode}<p>Temp: ${tempF.toFixed(2)}°F</p><p>Wind: ${windSpeed} MPH</p><p>Humidity: ${humidity}%</p>`).addClass('card-bg-blue');
             j += 8;
         }
+    };
 
+    function getIcon(cloudStatus) {
+        if (cloudStatus === 'Clouds') {
+            iconCode = '<img src="./assets/images/cloudy_rain_sun_icon.png" alt="Clouds">'
+        } else if (cloudStatus === 'Rain') {
+            iconCode = '<img src="./assets/images/rain_weather_icon.png" alt="Rain Clouds">'
+        } else if (cloudStatus === 'Snow') {
+            iconCode = '<img src="./assets/images/snow_icon.png" alt="Snow Clouds">'
+        } else {
+            iconCode = '<img src="./assets/images/sunny_weather_icon.png" alt="Sun Icon">'
+        }
+        return iconCode
     }
 
     btn.on('click', function () {
+        currentDay.html('');
         for (var i = 0; i < 5; i++) {
-            cardContainer.children().eq(i).children().eq(0).html('')
+            cardContainer.children().eq(i).children().eq(0).html('');
         }
         console.log('ok')
         var inputVal = cityInput.val();
@@ -123,8 +113,9 @@ $(document).ready(function () {
     );
 
     searchHistory.on('click', '#button', function () {
+        currentDay.html('');
         for (var i = 0; i < 5; i++) {
-            cardContainer.children().eq(i).children().eq(0).html('')
+            cardContainer.children().eq(i).children().eq(0).html('');
         }
         var inputVal = $(this).text();
         var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + inputVal + '&limit=5&appid=6c31869795f0dd512d210bae2ff7b924'
