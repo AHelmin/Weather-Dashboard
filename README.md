acb4737c1f896aba3543ff04a0ae798960192ac5
helm0127 <helm0127@morris.umn.edu>
2024-01-11 18:34:13 -0600

    commented css and js, completed readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 https://www.iconfinder.com/weather-icons?price=free source of the icons
+# Weather Dashboard
+
+## Description
+
+This project was created to create a web page capable of searching for cities around the world and displaying the current day forecast and a five day forecast.
+
+## Installation
+
+Installation is not required. The webpage is accessible from any web browser application.
+
+## Usage
+
+To use this service simply navigate to the website address below.
+
+https://ahelmin.github.io/Weather-Dashboard/
+
+To use the Weather Dashboard, type the name of the city in the search box and click the 'Search' button. The most recent eight cities that you have searched for will be listed as buttons on the left side of the screen. Just click these buttons to have the weather for these cities displayed again. 
+
+![Weather Dashboard Screenshot](./assets/images/Weather_Dashboard_Screenshot.png)
+
+## Credits
+
+Thanks goes to the University of Minnesota and Edx Bootcamp for providing the assistance needed to complete this project. A thanks goes out to Iconfinder (https://www.iconfinder.com/weather-icons?price=free) for the icons used in this application.
+
+## License
+
+MIT License
+
+Copyright (c) 2023 helm0127
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-//OFFIC HOUR QUESTIONS
-//ask about calling functions in the middle of another function
 
-
-//TODO
+//api addreww and my api key, will be left for future reference
 //https://api.openweathermap.org/geocoding?appid=859ytt99234r(api-key)
-//finish css to style page
 //my api key === 6c31869795f0dd512d210bae2ff7b924
 
+//function to ensure that this code will wait until the page is done loading
 $(document).ready(function () {
     const today = dayjs();
     const btn = $('#btn');
@@ -16,17 +13,25 @@ $(document).ready(function () {
     const currentDay = $('#current-day');
     const cardContainer = $('#five-day');
     const cardBody = $('#card-body');
+    let iconCode;
     let savedSearches = ['', '', '', '', '', '', '', ''];
 
+ //code block below was from the instructor, will be left here for future reference and for review
+    // let displayHistory = () => savedSearches.reverse().filter( (_,idx) => idx <= 3).forEach( (city) => 
+    //     searchHistory.append(`<button type="button" id="button">${city}</button>`))
+
+    //looks for saved data and if it exists writes the data to the savedSearches array
     function checkForSavedData() {
         const savedData = localStorage.getItem('savedSearches');
         if (savedData) {
             savedSearches = JSON.parse(savedData)
         }
-        displaySearchHistory();
+        displaySearchHistory()   
     };
 
+    //looks through savedSearches if they exist and appends buttons to page
     function displaySearchHistory() {
+        searchHistory.html("")
         for (var i = 0; i < savedSearches.length; i++) {
             console.log[savedSearches[i]]
             if (savedSearches[i] !== '') {
@@ -35,15 +40,29 @@ $(document).ready(function () {
         };
     };
 
+//code block below was from the instructor, will be left here for future reference and for review
+    // function displaySearchHistory2() {
+    //     searchHistory.html("")
+    //     savedSearches.reverse().forEach( function(city, idx) {
+    //         if( idx <= 3 ){
+    //             searchHistory.append(`<button type="button" id="button">${city}</button>`)
+    //         }
+    //     })
+    // };
+
+    
+    //saves the city name to savedSearches array if it is not already part of the saved cities, then writes the array to local storage
     function updateSavedData() {
+        if (!savedSearches.find( function(city){ city === cityInput.val()})) {
         savedSearches.unshift(cityInput.val());
+        }
         if (savedSearches.length > 8) {
             savedSearches.pop();
         }
         localStorage.setItem('savedSearches', JSON.stringify(savedSearches));
     };
 
-
+    // grabs the latitude and longitude of the city we queried then builds a url to be sent into the next api function
     async function getLocApi(requestUrl) {
         const response = await fetch(requestUrl);//   async await method
         const data = await response.json();
@@ -53,12 +72,14 @@ $(document).ready(function () {
         await getWeatherApi(weatherUrl);
     };
 
+    //uses the api built by the getLocApi funciton to find weather conditions in that area then sends data in the writeWeather function
     async function getWeatherApi(requestUrl) {
         const response = await fetch(requestUrl);//   async await method
         const data = await response.json();
         writeWeather(data);
     };
 
+    //looks at the data, pulls out desired info, formats, and writes this data to the webpage for users to see
     function writeWeather(data) {
         var dateTime = data.list[2].dt_txt;
         var adjustedDate = dayjs(dateTime).format('M/D/YYYY')
@@ -67,8 +88,7 @@ $(document).ready(function () {
         var tempF = (temp - 273.15) * 9 / 5 + 32;
         var humidity = data.list[2].main.humidity;
         var windSpeed = data.list[2].wind.speed;
-        console.log(cloudStatus);
-       getIcon(cloudStatus);
+       getIcon(cloudStatus);      
         var inputVal = cityInput.val();
         currentDay.append(`<h2>${inputVal} (${adjustedDate}) ${iconCode}</h2><p>Temp: ${tempF.toFixed(2)}°F</p><p>Wind: ${windSpeed} MPH</p><p>Humidity: ${humidity}%</p>`)
         var j = 2;
@@ -84,9 +104,13 @@ $(document).ready(function () {
             cardContainer.children().eq(i).children().eq(0).append(`<h5>${adjustedDate}</h5>${iconCode}<p>Temp: ${tempF.toFixed(2)}°F</p><p>Wind: ${windSpeed} MPH</p><p>Humidity: ${humidity}%</p>`).addClass('card-bg-blue');
             j += 8;
         }
+        //this function call is to ensure that the search history and buttons are updated even without a refresh from the user
+        displaySearchHistory()
     };
 
+    // checks the variable cloudStatus to assign a weather icon for the writeWeather function
     function getIcon(cloudStatus) {
+        
         if (cloudStatus === 'Clouds') {
             iconCode = '<img src="./assets/images/cloudy_rain_sun_icon.png" alt="Clouds">'
         } else if (cloudStatus === 'Rain') {
@@ -99,6 +123,7 @@ $(document).ready(function () {
         return iconCode
     }
 
+    // event listener for the search button
     btn.on('click', function () {
         currentDay.html('');
         for (var i = 0; i < 5; i++) {
@@ -112,6 +137,7 @@ $(document).ready(function () {
     }
     );
 
+    // event listener for the savedSearches buttons to make them search the respective city on mouseclick by constructing the api address for the getLocApi function
     searchHistory.on('click', '#button', function () {
         currentDay.html('');
         for (var i = 0; i < 5; i++) {
@@ -121,7 +147,8 @@ $(document).ready(function () {
         var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + inputVal + '&limit=5&appid=6c31869795f0dd512d210bae2ff7b924'
         cityInput.val(inputVal);
         getLocApi(requestUrl);
-    })
+    });
+    // initiates the js for the webpage
     checkForSavedData();
 
-})
+});
